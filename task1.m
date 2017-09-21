cc2060a5c9b628e5ce60fd99e3f8a7aa2ee70cc2
Your Name <you@example.com>
2017-09-21 09:48:22 +0400

    task1

--- task1.m
+++ task1.m
@@ -1,12 +1,14 @@
-a = 2; 
-b = 4.1;
-xn=0.77; 
-xk=1.77; 
-dx=0.2;
-X=[1.24,1.38,2.38,3.21,0.68]; 
+a = 2.0; 
+b = 3.0;
+xn=0.11; 
+xk=0.36; 
+dx=0.05;
+X=[0.08,0.26,0.35,0.41,0.53]; 
 X=sort(X);
 x=xn:dx:xk;
-
+function yres=yCalc(x,a,b)
+  yres = asin(x.^a)+ acos(x.^b);
+end
 y1 = yCalc(x,a,b);
 y2 = yCalc(X,a,b);
 
@@ -22,8 +24,3 @@ plot(X,y2,'b-*','LineWidth',3)
 title('First lab part B')
 
 
-function yres=yCalc(x,a,b)
-  chisl = log(b^2 - x.^2)/log(a);
-  znam = (abs(x.^2 - a^2)).^(1/3);
-  yres = chisl./znam;
-end
